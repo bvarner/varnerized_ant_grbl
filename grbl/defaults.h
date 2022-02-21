d4ba46fcd2e6884a8996a0340406281b35e2a79f
Bryan Varner <1652015+bvarner@users.noreply.github.com>
2022-02-21 14:54:25 -0500

    Updates. Moved the XY to 1/8th stepping (as originally specified by ANT docs) and adjusted max rates / spindle RPM.

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -29,11 +29,11 @@
 
 #ifdef DEFAULTS_GENERIC
   // Grbl generic default settings. Should work across different machines.
-  #define DEFAULT_X_STEPS_PER_MM 206.51
-  #define DEFAULT_Y_STEPS_PER_MM 206.51
+  #define DEFAULT_X_STEPS_PER_MM 100
+  #define DEFAULT_Y_STEPS_PER_MM 100
   #define DEFAULT_Z_STEPS_PER_MM 631.313131313
-  #define DEFAULT_X_MAX_RATE 1000.0 // mm/min
-  #define DEFAULT_Y_MAX_RATE 1000.0 // mm/min
+  #define DEFAULT_X_MAX_RATE 6000.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 6000.0 // mm/min
   #define DEFAULT_Z_MAX_RATE 500.0 // mm/min
   #define DEFAULT_X_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Y_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
@@ -42,7 +42,7 @@
   #define DEFAULT_Y_MAX_TRAVEL 110.0 // mm NOTE: Must be a positive value.
   #define DEFAULT_Z_MAX_TRAVEL 24.0 // mm NOTE: Must be a positive value.
   #define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
-  #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
+  #define DEFAULT_SPINDLE_RPM_MIN 1.0 // rpm
   #define DEFAULT_STEP_PULSE_MICROSECONDS 10
   #define DEFAULT_STEPPING_INVERT_MASK 0
   #define DEFAULT_DIRECTION_INVERT_MASK 0
